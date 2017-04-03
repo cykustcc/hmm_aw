61fc31f945ee36b259fba18a6bea89f6952b24a0
Yukun Chen <cykustc@gmail.com>
2017-04-03 16:37:48 -0400

    update

--- src/hmmdist.cpp
+++ src/hmmdist.cpp
@@ -5,5 +5,44 @@
 //  Created by Yukun Chen on 3/27/17.
 //  Copyright © 2017 cyk. All rights reserved.
 //
+#include "glog/logging.h"
+#include "hmm.h"
+#include <random>
+#include <cassert>
+#include <map>
+#include "utils/lapacke.h"
 
-#include <stdio.h>
+double HmmModel::dist_KL(HmmModel &hmm2){
+  return 0.0;
+}
+
+double HmmModel::dist_MAW(HmmModel &hmm2){
+  return 0.0;
+}
+
+double HmmModel::dist_IAW(HmmModel &hmm2){
+  return 0.0;
+}
+
+void HmmModel::gen_seq(std::vector<float> &seq, int n){
+  assert(seq.size() == n);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::map<int, int> m;
+  std::vector<std::discrete_distribution<>> state_dd;
+  std::discrete_distribution<> init_state_dd(this->a00.begin(), this->a00.end());
+  for (int i=0; i<numst; i++) {
+    std::discrete_distribution<> tmp(this->a[i].begin(), this->a[i].end());
+    state_dd.push_back(tmp);
+  }
+  int cur_state = init_state_dd(gen);
+  for(int i = 0; i < n; i++){
+    double cumulate = 0.0;
+    cur_state = state_dd[cur_state](gen);
+    
+    m[cur_state]++;
+  }
+  for(auto p : m) {
+    std::cout << p.first << " generated " << p.second << " times\n";
+  }
+};
--- test/hmmdist_test.cpp
+++ test/hmmdist_test.cpp
@@ -5,5 +5,27 @@
 //  Created by Yukun Chen on 3/27/17.
 //  Copyright © 2017 cyk. All rights reserved.
 //
+#include "gtest/gtest.h"
+#include "glog/logging.h"
+#include <hmm.h>
+#include "test_common.h"
 
-#include <stdio.h>
+class HMMDistTest: public ::testing::Test{
+protected:
+  HmmModel gt_hmm;
+  virtual void SetUp(){
+    std::string filenamein = root_path + "/data/test/hmm_forhmmfittest.in";
+    gt_hmm.read_model(filenamein);
+  }
+  virtual void TearDown()
+  {
+    
+  }
+};
+
+TEST_F(HMMDistTest, GenSeq){
+  int n = 200;
+  std::vector<float> seq(n, 0.0);
+  gt_hmm.gen_seq(seq, n);
+}
+
